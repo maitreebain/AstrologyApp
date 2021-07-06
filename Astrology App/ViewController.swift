cd846a7ef49f9ca34e059f6577767de312656c3e
maitreebain <maitreebain@gmail.com>
2021-07-05 21:47:55 -0400

    Initial Commit

---
+++ Astrology App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Astrology App
+//
+//  Created by Maitree Bain on 7/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
