61cb701ed95eb7b5a1e75f60bee7bb0f18ac7d1a
maitreebain <maitreebain@gmail.com>
2021-07-08 17:35:37 -0400

    added api client

--- Astrology App/Models + APIClients/AstrologyAPIClient.swift
+++ Astrology App/Models + APIClients/AstrologyAPIClient.swift
@@ -5,12 +5,33 @@
 //  Created by Maitree Bain on 7/6/21.
 //
 
+import UIKit
 import Foundation
 
 struct AstrologyAPIClient {
     
-//    func getSign(sign: String, completion: @escaping (Result<AstrologyModel, Error>) -> ()) {
-//
-//    let endpoint = "http://sandipbgt.com/theastrologer/api/horoscope/\(sign)/today"
-//    }
+    func getSign(sign: String, completion: @escaping (Result<AstrologyModel, Error>) -> ()) {
+        
+        let endpoint = "http://sandipbgt.com/theastrologer/api/horoscope/\(sign)/today"
+        
+        guard let url = URL(string: endpoint) else {
+            print("no url found")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let datatask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    let zodiac = try JSONDecoder().decode(AstrologyModel.self, from: data)
+                    completion(.success(zodiac))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        datatask.resume()
+    }
 }
